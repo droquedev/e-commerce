760fc5a725f72e0377e403fb4fc8369dae7d8c86
droquedev <davidroque21@hotmail.com>
2024-02-06 18:53:30 -0400

    chore: move main file for product service

---
+++ cmd/products-service/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"github.com/nats-io/stan.go"
+)
+
+const (
+	host     = "postgres"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "productsdb"
+)
+
+var natsConn stan.Conn
+var db *sql.DB
+
+func init() {
+	var err error
+	time.Sleep(5 * time.Second)
+	natsConn, err = stan.Connect("test-cluster", "products-service", stan.NatsURL(os.Getenv("NATS_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = natsConn.Subscribe("user:created", func(msg *stan.Msg) {
+		log.Printf("Received user.created event: %s", string(msg.Data))
+		msg.Ack()
+		// Handle the event as needed
+	}, stan.StartAt(0), stan.SetManualAckMode())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the PostgreSQL database")
+}
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+var products []Product = []Product{
+	{
+		ID:    1,
+		Name:  "Cheese",
+		Price: 500,
+	},
+	{
+		ID:    2,
+		Name:  "Milk",
+		Price: 200,
+	},
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/products", getProducts)
+	router.POST("/products", addProduct)
+
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, products)
+}
+
+func addProduct(c *gin.Context) {
+	var product Product
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	product.ID = len(products) + 1
+	products = append(products, product)
+
+	c.JSON(http.StatusOK, product)
+}
